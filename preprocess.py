c53c4ec7249e7798045920aa63d10fea795efc76
kerengaiger <keren.gaiger@gmail.com>
2021-03-30 21:27:46 +0300

    Preprocess: add precprocess for test file

--- preprocess.py
+++ preprocess.py
@@ -11,12 +11,14 @@ def parse_args():
     parser.add_argument('--data_dir', type=str, default='./data/', help="data directory path")
     parser.add_argument('--vocab', type=str, default='./data/corpus.txt', help="corpus path for building vocab")
     parser.add_argument('--full_corpus', type=str, default='./data/corpus.txt', help="corpus path")
-    parser.add_argument('--train_corpus', type=str, default='./data/train_corpus.txt', help="train corpus path")
-    parser.add_argument('--build_valid', action='store_true', help="build validation set from validation users file")
-    parser.add_argument('--valid_corpus', type=str, default='./data/valid_corpus_avi.txt', help="source path to build validation set, in case of build_valid")
+    parser.add_argument('--test_corpus', type=str, default='./data/test_corpus_avi.txt', help="test corpus path")
+    parser.add_argument('--build_train_valid', action='store_true', help="build part train and validation sets from provided paths")
+    parser.add_argument('--train_corpus', type=str, default='./data/train_corpus.txt', help="part train corpus path to build part train set, in case build_train_valid is True")
+    parser.add_argument('--valid_corpus', type=str, default='./data/valid_corpus_avi.txt', help="validation corpys path to build validation set, in case build_train_valid is True")
     parser.add_argument('--full_train_file', type=str, default='./data/full_train.dat', help="full train file name")
-    parser.add_argument('--train_file', type=str, default='./data/train.dat', help="train file name")
-    parser.add_argument('--valid_file', type=str, default='./data/valid_avi.dat', help="validation file name, in case of build_valid")
+    parser.add_argument('--train_file', type=str, default='./data/train.dat', help="train file name, in case of build_valid")
+    parser.add_argument('--valid_file', type=str, default='./data/valid.dat', help="validation file name, in case of build_valid")
+    parser.add_argument('--test_file', type=str, default='./data/test.dat', help="test file of sub users and target items")
     parser.add_argument('--unk', type=str, default='<UNK>', help="UNK token")
     parser.add_argument('--max_vocab', type=int, default=20000, help="maximum number of vocab")
     return parser.parse_args()
@@ -88,14 +90,49 @@ class Preprocess(object):
         print("conversion done")
         print("num of users:", num_users)
 
+    def process_test(self, test_path, savepath):
+        print("processing test...")
+        step = 0
+        data = []
+        with codecs.open(test_path, 'r', encoding='utf-8') as file:
+            num_users = 0
+            for line in file:
+                step += 1
+                if not step % 1000:
+                    print("working on {}kth line".format(step // 1000), end='\r')
+                line = line.strip()
+                if not line:
+                    continue
+                user = []
+                for item in line.split():
+                    if item in self.vocab:
+                        user.append(item)
+                    else:
+                        user.append(self.unk)
+                if len(user) < 2:
+                    print('skip user')
+                    continue
+                num_users += 1
+                # create list of [sub-user: lst[item ids], target_item_id]
+                for i in range(1, len(user)):
+                    sub_user = user[:i]
+                    target_item = user[i]
+                    data.append(([self.item2idx[item] for item in sub_user], self.item2idx[target_item]))
+
+        print("")
+        pickle.dump(data, open(savepath, 'wb'))
+        print("conversion done")
+        print("num of users:", num_users)
+
 
 def main():
     args = parse_args()
     preprocess = Preprocess(unk=args.unk, data_dir=args.data_dir)
     preprocess.build(args.vocab, max_vocab=args.max_vocab)
     preprocess.convert(args.full_corpus, args.full_train_file)
-    preprocess.convert(args.train_corpus, args.train_file)
-    if args.build_valid:
+    preprocess.process_test(args.test_corpus, args.test_file)
+    if args.build_train_valid:
+        preprocess.convert(args.train_corpus, args.train_file)
         preprocess.convert(args.valid_corpus, args.valid_file)
 
 
