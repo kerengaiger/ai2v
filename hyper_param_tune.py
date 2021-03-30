4713084ca4af8f775ee96638a80fa9be335e5708
kerengaiger <keren.gaiger@gmail.com>
2021-03-30 20:18:04 +0300

    Tune: remove k and hr_weight. change conversion thresh and max epochs defaults

--- hyper_param_tune.py
+++ hyper_param_tune.py
@@ -16,12 +16,10 @@ def parse_args():
     parser.add_argument('--train', type=str, default='train.dat', help="train file name")
     parser.add_argument('--valid', type=str, default='valid_avi.dat', help="validation users file name")
     parser.add_argument('--full_train', type=str, default='full_train.dat', help="full train file name")
-    parser.add_argument('--max_epoch', type=int, default=100, help="max number of epochs")
-    parser.add_argument('--k', type=int, default=20, help="number of top ranked items")
-    parser.add_argument('--conv_thresh', type=float, default=0.0001, help="threshold diff for convergence")
+    parser.add_argument('--max_epoch', type=int, default=50, help="max number of epochs")
+    parser.add_argument('--conv_thresh', type=float, default=0.001, help="threshold diff for convergence")
     parser.add_argument('--patience', type=float, default=3, help="epochs to wait until early stopping")
     parser.add_argument('--unk', type=str, default='<UNK>', help="UNK token")
-    parser.add_argument('--hrk_weight', type=float, default=0.5, help="weight to put on hrk metric value")
     parser.add_argument('--trials', type=int, default=10, help="number of trials ")
     parser.add_argument('--cuda', action='store_true', help="use CUDA")
     parser.add_argument('--max_batch_size', type=int, default=300, help="max number of training obs in batch")
@@ -46,10 +44,8 @@ def main():
             {"name": "mini_batch", "type": "choice", "value_type": "int", "values": [100, 90]},
             {"name": "weights", "type": "choice", "value_type": "bool", "values": [False, False]},
             {"name": "max_epoch", "type": "fixed", "value_type": "int", "value": args.max_epoch},
-            {"name": "k", "type": "fixed", "value_type": "int", "value": args.k},
             {"name": "patience", "type": "fixed", "value_type": "int", "value": args.patience},
             {"name": "conv_thresh", "type": "fixed", "value_type": "float", "value": args.conv_thresh},
-            {"name": "hrk_weight", "type": "fixed", "value_type": "float", "value": args.hrk_weight},
             {"name": "unk", "type": "fixed", "value_type": "str", "value": args.unk},
             {"name": "cuda", "type": "fixed", "value": args.cuda},
             {"name": "data_dir", "type": "fixed", "value_type": "str", "value": args.data_dir},
