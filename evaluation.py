b024b70980ed387638cccdb9cff2f5f8b094b804
kerengaiger <keren.gaiger@gmail.com>
2021-03-30 21:36:33 +0300

    Preprocess: fix numpy array shapes of user representation and item representations

--- evaluation.py
+++ evaluation.py
@@ -5,6 +5,14 @@ import argparse
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--k', type=int, default=20, help="k for hr and mrr")
+    parser.add_argument('--model', type=str, default='./output/best_model.pt', help="best model trained")
+    parser.add_argument('--test', type=str, default='./data/test.dat', help="test set for evaluation")
+    return parser.parse_args()
+
+
 def represent_user(user_itemids, model):
     context_vecs = model.embedding.ivectors.weight.data.cpu().numpy()
     user2vec = context_vecs[user_itemids, :].mean(axis=0)
@@ -24,7 +32,7 @@ def mrr_k(model, eval_set, k):
 
 
 def inference(model, user_itemids):
-    user2vec = represent_user(user_itemids, model)
+    user2vec = np.expand_dims(represent_user(user_itemids, model), axis=0)
     user_sim = cosine_similarity(user2vec, model.embedding.ovectors.weight.data.cpu().numpy()).squeeze()
     return user_sim.argsort()
 
@@ -37,14 +45,6 @@ def hr_k(model, eval_set, k):
         if target_item in top_k_items:
             in_top_k += 1
     return in_top_k / len(eval_set)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--k', type=int, default=20, help="k for hr and mrr")
-    parser.add_argument('--model', type=str, default='./data/best_model.pt', help="best model trained")
-    parser.add_argument('--test', type=str, default='./data/test.dat', help="test set for evaluation")
-    return parser.parse_args()
 
 
 def main():
