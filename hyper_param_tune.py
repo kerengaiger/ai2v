460e2d0af123898c36996acc48689336327c52bd
kerengaiger <keren.gaiger@gmail.com>
2021-04-10 12:56:02 +0300

    Tune: ass modularity for i2v and ai2v

--- hyper_param_tune.py
+++ hyper_param_tune.py
@@ -5,11 +5,18 @@ import argparse
 
 from ax.service.managed_loop import optimize
 
-from train import train_evaluate, train
+from train_i2v import train_evaluate as train_evaluate_i2v
+from train_i2v import train as train_i2v
+from train_ai2v import train_evaluate as train_evaluate_ai2v
+from train_ai2v import train as train_ai2v
+
+I2V = 'i2v'
+AI2V = 'ai2v'
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='ai2v', help="model to train: i2v or ai2v")
     parser.add_argument('--name', type=str, default='sgns', help="model name")
     parser.add_argument('--data_dir', type=str, default='./data/', help="data directory path")
     parser.add_argument('--save_dir', type=str, default='./output/', help="model directory path")
@@ -27,40 +34,75 @@ def parse_args():
     return parser.parse_args()
 
 
-def full_train(cnfg, epochs, args):
+def i2v_full_train(cnfg, epochs, args):
     cnfg['max_epoch'] = int(epochs)
     cnfg['train'] = args.full_train
-    train(cnfg)
+    train_i2v(cnfg)
+
+
+def ai2v_full_train(cnfg, epochs, args):
+    cnfg['max_epoch'] = int(epochs)
+    cnfg['train'] = args.full_train
+    train_ai2v(cnfg)
 
 
 def main():
     args = parse_args()
-    best_parameters, values, _experiment, _cur_model = optimize(
-        parameters=[
-            {"name": "lr", "type": "range", "value_type": "float", "bounds": [3e-2, 1e-1]},
-            {"name": "ss_t", "type": "range", "value_type": "float", "bounds": [1e-5, 3e-3]},
-            {"name": "e_dim", "type": "choice", "value_type": "int", "values": [12, 17, 20, 25, 30]},
-            {"name": "n_negs", "type": "choice", "value_type": "int", "values": [5, 7, 10, 15]},
-            {"name": "mini_batch", "type": "choice", "value_type": "int", "values": [100, 90]},
-            {"name": "weights", "type": "choice", "value_type": "bool", "values": [False, False]},
-            {"name": "max_epoch", "type": "fixed", "value_type": "int", "value": args.max_epoch},
-            {"name": "patience", "type": "fixed", "value_type": "int", "value": args.patience},
-            {"name": "conv_thresh", "type": "fixed", "value_type": "float", "value": args.conv_thresh},
-            {"name": "unk", "type": "fixed", "value_type": "str", "value": args.unk},
-            {"name": "cuda", "type": "fixed", "value": args.cuda},
-            {"name": "data_dir", "type": "fixed", "value_type": "str", "value": args.data_dir},
-            {"name": "save_dir", "type": "fixed", "value_type": "str", "value": args.save_dir},
-            {"name": "train", "type": "fixed", "value_type": "str", "value": args.train},
-            {"name": "valid", "type": "fixed", "value_type": "str", "value": args.valid},
-            {"name": "max_batch_size", "type": "fixed", "value_type": "int", "value": args.max_batch_size},
-        ],
-        evaluation_function=train_evaluate,
-        minimize=True,
-        objective_name='valid_loss',
-        total_trials=args.trials
-    )
+    if args.model == I2V:
+        best_parameters, values, _experiment, _cur_model = optimize(
+            parameters=[
+                {"name": "lr", "type": "range", "value_type": "float", "bounds": [3e-2, 1e-1]},
+                {"name": "ss_t", "type": "range", "value_type": "float", "bounds": [1e-5, 3e-3]},
+                {"name": "e_dim", "type": "choice", "value_type": "int", "values": [12, 17, 20, 25, 30]},
+                {"name": "n_negs", "type": "choice", "value_type": "int", "values": [5, 7, 10, 15]},
+                {"name": "mini_batch", "type": "choice", "value_type": "int", "values": [100, 90]},
+                {"name": "weights", "type": "choice", "value_type": "bool", "values": [False, False]},
+                {"name": "max_epoch", "type": "fixed", "value_type": "int", "value": args.max_epoch},
+                {"name": "patience", "type": "fixed", "value_type": "int", "value": args.patience},
+                {"name": "conv_thresh", "type": "fixed", "value_type": "float", "value": args.conv_thresh},
+                {"name": "unk", "type": "fixed", "value_type": "str", "value": args.unk},
+                {"name": "cuda", "type": "fixed", "value": args.cuda},
+                {"name": "data_dir", "type": "fixed", "value_type": "str", "value": args.data_dir},
+                {"name": "save_dir", "type": "fixed", "value_type": "str", "value": args.save_dir},
+                {"name": "train", "type": "fixed", "value_type": "str", "value": args.train},
+                {"name": "valid", "type": "fixed", "value_type": "str", "value": args.valid},
+                {"name": "max_batch_size", "type": "fixed", "value_type": "int", "value": args.max_batch_size},
+            ],
+            evaluation_function=train_evaluate_i2v,
+            minimize=True,
+            objective_name='valid_loss',
+            total_trials=args.trials
+        )
 
-    full_train(best_parameters, values[0]['early_stop_epoch'], args)
+        i2v_full_train(best_parameters, values[0]['early_stop_epoch'], args)
+
+    else:
+        best_parameters, values, _experiment, _cur_model = optimize(
+            parameters=[
+                {"name": "lr", "type": "range", "value_type": "float", "bounds": [3e-2, 1e-1]},
+                {"name": "ss_t", "type": "range", "value_type": "float", "bounds": [1e-5, 3e-3]},
+                {"name": "e_dim", "type": "choice", "value_type": "int", "values": [12, 17, 20, 25, 30]},
+                {"name": "n_negs", "type": "choice", "value_type": "int", "values": [5, 7, 10, 15]},
+                {"name": "mini_batch", "type": "choice", "value_type": "int", "values": [100, 90]},
+                {"name": "weights", "type": "choice", "value_type": "bool", "values": [False, False]},
+                {"name": "max_epoch", "type": "fixed", "value_type": "int", "value": args.max_epoch},
+                {"name": "patience", "type": "fixed", "value_type": "int", "value": args.patience},
+                {"name": "conv_thresh", "type": "fixed", "value_type": "float", "value": args.conv_thresh},
+                {"name": "unk", "type": "fixed", "value_type": "str", "value": args.unk},
+                {"name": "cuda", "type": "fixed", "value": args.cuda},
+                {"name": "data_dir", "type": "fixed", "value_type": "str", "value": args.data_dir},
+                {"name": "save_dir", "type": "fixed", "value_type": "str", "value": args.save_dir},
+                {"name": "train", "type": "fixed", "value_type": "str", "value": args.train},
+                {"name": "valid", "type": "fixed", "value_type": "str", "value": args.valid},
+                {"name": "max_batch_size", "type": "fixed", "value_type": "int", "value": args.max_batch_size},
+            ],
+            evaluation_function=train_evaluate_ai2v,
+            minimize=True,
+            objective_name='valid_loss',
+            total_trials=args.trials
+        )
+
+        ai2v_full_train(best_parameters, values[0]['early_stop_epoch'], args)
 
 
 if __name__ == '__main__':
