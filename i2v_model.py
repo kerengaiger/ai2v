35b2f5fdf3ed9678bbf8ae5d466a3110046ffe3b
kerengaiger <keren.gaiger@gmail.com>
2021-04-13 16:28:13 +0300

    i2v_model: changed o and i to c and t respectively.

--- i2v_model.py
+++ i2v_model.py
@@ -27,8 +27,8 @@ class Item2Vec(Bundler):
         self.name = 'i2v'
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
-        self.ivectors = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=padding_idx)
-        self.ovectors = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=padding_idx)
+        self.tvectors = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=padding_idx)
+        self.cvectors = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=padding_idx)
         self.ivectors.weight = nn.Parameter(t.cat([t.zeros(1, self.embedding_size), FT(self.vocab_size - 1, self.embedding_size).uniform_(-0.5 / self.embedding_size, 0.5 / self.embedding_size)]))
         self.ovectors.weight = nn.Parameter(t.cat([t.zeros(1, self.embedding_size), FT(self.vocab_size - 1, self.embedding_size).uniform_(-0.5 / self.embedding_size, 0.5 / self.embedding_size)]))
         self.ivectors.weight.requires_grad = True
@@ -37,17 +37,17 @@ class Item2Vec(Bundler):
     def forward(self, data):
         return self.forward_i(data)
 
-    def forward_i(self, data):
+    def forward_t(self, data):
         # v = LT(data)
         v = data.long()
-        v = v.cuda() if self.ivectors.weight.is_cuda else v
-        return self.ivectors(v)
+        v = v.cuda() if self.tvectors.weight.is_cuda else v
+        return self.tvectors(v)
 
-    def forward_o(self, data):
+    def forward_c(self, data):
         # v = LT(data)
         v = data.long()
-        v = v.cuda() if self.ovectors.weight.is_cuda else v
-        return self.ovectors(v)
+        v = v.cuda() if self.cvectors.weight.is_cuda else v
+        return self.cvectors(v)
 
 
 class SGNS(nn.Module):
@@ -63,34 +63,34 @@ class SGNS(nn.Module):
             wf = wf / wf.sum()
             self.weights = FT(wf)
 
-    def forward(self, iitem, oitems):
-        batch_size = iitem.size()[0]
-        context_size = oitems.size()[1]
+    def forward(self, titems, citems):
+        batch_size = titems.size()[0]
+        context_size = citems.size()[1]
         if self.weights is not None:
             nitems = t.multinomial(self.weights, batch_size * context_size * self.n_negs, replacement=True).view(batch_size, -1)
         else:
             nitems = FT(batch_size, context_size * self.n_negs).uniform_(0, self.vocab_size - 1).long()
-        ivectors = self.embedding.forward_i(iitem).unsqueeze(2)
+        tvectors = self.embedding.forward_t(titems).unsqueeze(2)
         # print('ivectors', ivectors.shape)
         # print('ivectors', ivectors)
 
         # print('ivectors', ivectors.shape)
-        ovectors = self.embedding.forward_o(oitems)
+        cvectors = self.embedding.forward_c(citems)
         # print('ovectors', ovectors.shape)
         # print('ovectors', ovectors)
 
         # print('ovectors', ovectors.shape)
-        nvectors = self.embedding.forward_o(nitems).neg()
+        nvectors = self.embedding.forward_t(nitems).neg()
         # print('nvectors', nvectors.shape)
         # print('mult o and i', t.bmm(ovectors, ivectors).shape)
-        oloss = t.bmm(ovectors, ivectors).squeeze(dim=-1).sigmoid().log()
+        tloss = t.bmm(cvectors, tvectors).squeeze(dim=-1).sigmoid().log()
         # print('oloss', oloss.shape)
         # print('oloss', oloss.shape)
-        assert oloss.shape[0] == batch_size, 'oloss vector shape is different than batch size'
-        nloss = t.bmm(nvectors, ivectors).squeeze(dim=-1).sigmoid().log().view(-1, context_size, self.n_negs).sum(2)
+        assert tloss.shape[0] == batch_size, 'tloss vector shape is different than batch size'
+        nloss = t.bmm(nvectors, tvectors).squeeze(dim=-1).sigmoid().log().view(-1, context_size, self.n_negs).sum(2)
         # print('mult n and i', t.bmm(nvectors, ivectors).shape)
         # print('nloss', nloss.shape)
         assert nloss.shape[0] == batch_size, 'nloss vector shape is different than batch size'
-        loss = oloss + nloss
+        loss = tloss + nloss
         loss = loss.sum(1).mean()
         return -loss
