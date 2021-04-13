843ba282fc112420284518573813ebdc204dc385
kerengaiger <keren.gaiger@gmail.com>
2021-04-13 13:01:48 +0300

    train_utils: add a shared Dataset class

--- train_utils.py
+++ train_utils.py
@@ -4,10 +4,31 @@ import random
 
 import numpy as np
 import torch as t
-from torch.optim import Adagrad
-from torch.utils.data import Dataset, DataLoader
-from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
+from torch.utils.data import Dataset
+
+
+class UserBatchIncrementDataset(Dataset):
+    def __init__(self, datapath, pad_idx, window_size, ws=None):
+        data = pickle.load(datapath.open('rb'))
+        if ws is not None:
+            data_ws = []
+            for citems, titem in data:
+                if random.random() > ws[titem]:
+                    data_ws.append((citems, titem))
+            data = data_ws
+
+        padded_data = []
+        for sub_user in data:
+            padded_data.append((sub_user[0] + [pad_idx for _ in range(window_size - len(sub_user[0]))], sub_user[1]))
+
+        self.data = padded_data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        citems, titem = self.data[idx]
+        return titem, np.array(citems)
 
 
 def configure_weights(cnfg, idx2item):
@@ -34,3 +55,6 @@ def save_model(cnfg, model, sgns):
     pickle.dump(ivectors, open(pathlib.Path(cnfg['save_dir'], 'idx2ivec.dat'), 'wb'))
     pickle.dump(ovectors, open(pathlib.Path(cnfg['save_dir'], 'idx2ovec.dat'), 'wb'))
     t.save(sgns, pathlib.Path(cnfg['save_dir'], 'best_model.pt'))
+
+
+
