83ead540b2d2cada2f8c6b580fe9abfc42c85591
kerengaiger <keren.gaiger@gmail.com>
2021-04-20 11:43:04 +0300

    Evaluation_i2v: remove inference and represent_user and create them as model functions. ass call to mrr in main

--- evaluation_i2v.py
+++ evaluation_i2v.py
@@ -2,7 +2,6 @@ import torch as t
 import pickle
 import numpy as np
 import argparse
-from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 
 
@@ -14,27 +13,18 @@ def parse_args():
     return parser.parse_args()
 
 
-def represent_user(user_itemids, model):
-    context_vecs = model.embedding.ivectors.weight.data.cpu().numpy()
-    user2vec = context_vecs[user_itemids, :].mean(axis=0)
-    return user2vec
-
-
 def mrr_k(model, eval_set, k):
     in_top_k, rec_rank = 0, 0
-    for user_itemids, target_item in eval_set:
-        items_ranked = inference(model, user_itemids)
-        top_k_items = items_ranked.argsort()[-k:][::-1]
+
+    pbar = tqdm(eval_set)
+
+    for user_itemids, target_item in pbar:
+        items_ranked = model.inference(user_itemids).argsort()
+        top_k_items = items_ranked[-k:][::-1]
         if target_item in top_k_items:
             in_top_k += 1
             rec_rank += 1 / (np.where(top_k_items == target_item)[0][0] + 1)
     return rec_rank / in_top_k
-
-
-def inference(model, user_itemids):
-    user2vec = np.expand_dims(represent_user(user_itemids, model), axis=0)
-    user_sim = cosine_similarity(user2vec, model.embedding.ovectors.weight.data.cpu().numpy()).squeeze()
-    return user_sim
 
 
 def hr_k(model, eval_set, k):
@@ -43,7 +33,7 @@ def hr_k(model, eval_set, k):
     pbar = tqdm(eval_set)
 
     for user_itemids, target_item in pbar:
-        items_ranked = inference(model, user_itemids).argsort()
+        items_ranked = model.inference(user_itemids).argsort()
         top_k_items = items_ranked[-k:][::-1]
         if target_item in top_k_items:
             in_top_k += 1
@@ -55,6 +45,7 @@ def main():
     model = t.load(args.model)
     eval_set = pickle.load(open(args.test, 'rb'))
     print(f'hit ratio at {args.k}:', hr_k(model, eval_set, args.k))
+    print(f'mrr at {args.k}:', mrr_k(model, eval_set, args.k))
 
 
 if __name__ == '__main__':
