787041ad4cd54d1eca0abc4cd29cae7508287a18
kerengaiger <keren.gaiger@gmail.com>
2021-04-30 18:04:05 +0300

    train_ai2v: bix best model load

--- train_ai2v.py
+++ train_ai2v.py
@@ -151,7 +151,7 @@ def train_evaluate(cnfg):
 
     best_epoch = train_early_stop(cnfg, valid_users_path, item2idx['pad'])
 
-    best_model = t.load(pathlib.Path(cnfg['save_dir'], 'best_model.pt'))
+    best_model = t.load(pathlib.Path(cnfg['save_dir'], cnfg['model'] + '_best.pt'))
 
     valid_loss = calc_loss_on_set(best_model, valid_users_path, item2idx['pad'], cnfg['mini_batch'], cnfg['window_size'])
     return {'valid_loss': (valid_loss, 0.0), 'early_stop_epoch': (best_epoch, 0.0)}
