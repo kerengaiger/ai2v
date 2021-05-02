4f7334cb46c8cdc14808fe7f50d128fc2ea810ba
kerengaiger <keren.gaiger@gmail.com>
2021-05-02 12:46:56 +0300

    ai2v_model: add abs after the diff between u and v

--- ai2v_model.py
+++ ai2v_model.py
@@ -88,7 +88,7 @@ class SGNS(nn.Module):
     def similarity(self, batch_sub_users, batch_tvecs, batch_titem_ids):
         return self.ai2v.W1(self.ai2v.relu(self.ai2v.W0(t.cat([batch_sub_users, batch_tvecs,
                                                         t.mul(batch_sub_users, batch_tvecs),
-                                                        batch_sub_users - batch_tvecs], 2)))) + \
+                                                        (batch_sub_users - batch_tvecs).abs()], 2)))) + \
             self.ai2v.b_l_j[batch_titem_ids].unsqueeze(2)
 
     def inference(self, user_items):
