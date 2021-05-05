0855015ecd843482b4e6cd5358b9764213a5f09b
kerengaiger <keren.gaiger@gmail.com>
2021-05-05 13:05:28 +0300

    Preprocess: remove ai2_preprocess.py and rename i2v_preprocess to preprocess - the same processing for both models

--- preprocess.py
+++ preprocess.py
@@ -15,10 +15,10 @@ def parse_args():
     parser.add_argument('--build_train_valid', action='store_true', help="build part train and validation sets from provided paths")
     parser.add_argument('--train_corpus', type=str, default='./data/train_corpus_avi.txt', help="part train corpus path to build part train set, in case build_train_valid is True")
     parser.add_argument('--valid_corpus', type=str, default='./data/valid_corpus_avi.txt', help="validation corpys path to build validation set, in case build_train_valid is True")
-    parser.add_argument('--full_train_file', type=str, default='./data/full_train_ai2v_batch_u.dat', help="full train file name")
-    parser.add_argument('--train_file', type=str, default='./data/train_ai2v_batch_u.dat', help="train file name, in case of build_valid")
-    parser.add_argument('--valid_file', type=str, default='./data/valid_ai2v_batch_u.dat', help="validation file name, in case of build_valid")
-    parser.add_argument('--test_file', type=str, default='./data/test_ai2v_batch_u.dat', help="test file of sub users and target items")
+    parser.add_argument('--full_train_file', type=str, default='./data/full_train_batch_u.dat', help="full train file name")
+    parser.add_argument('--train_file', type=str, default='./data/train_batch_u.dat', help="train file name, in case of build_valid")
+    parser.add_argument('--valid_file', type=str, default='./data/valid_batch_u.dat', help="validation file name, in case of build_valid")
+    parser.add_argument('--test_file', type=str, default='./data/test_batch_u.dat', help="test file of sub users and target items")
     parser.add_argument('--unk', type=str, default='<UNK>', help="UNK token")
     parser.add_argument('--max_vocab', type=int, default=20000, help="maximum number of vocab")
     return parser.parse_args()
@@ -60,12 +60,7 @@ class Preprocess(object):
         pickle.dump(self.item2idx, open(os.path.join(self.data_dir, 'item2idx.dat'), 'wb'))
         print("build done")
 
-    def create_sub_user(self, user, item_target):
-        sub_user = user[:item_target]
-        target_item = user[item_target]
-        return [self.item2idx[item] for item in sub_user], self.item2idx[target_item]
-
-    def convert(self, filepath, savepath, test=False):
+    def convert(self, filepath, savepath):
         print("converting corpus...")
         step = 0
         data = []
@@ -88,14 +83,41 @@ class Preprocess(object):
                     print('skip user')
                     continue
                 num_users += 1
-                if test:
-                    for item_target in range(1, len(user)):
-                        data.append((self.create_sub_user(user, item_target)))
-                else:
-                    user_subs = []
-                    for item_target in range(1, len(user)):
-                        user_subs.append((self.create_sub_user(user, item_target)))
-                    data.append(user_subs)
+                data.append([self.item2idx[item] for item in user])
+
+        print("")
+        pickle.dump(data, open(savepath, 'wb'))
+        print("conversion done")
+        print("num of users:", num_users)
+
+    def process_test(self, test_path, savepath):
+        print("processing test...")
+        step = 0
+        data = []
+        with codecs.open(test_path, 'r', encoding='utf-8') as file:
+            num_users = 0
+            for line in file:
+                step += 1
+                if not step % 1000:
+                    print("working on {}kth line".format(step // 1000), end='\r')
+                line = line.strip()
+                if not line:
+                    continue
+                user = []
+                for item in line.split():
+                    if item in self.vocab:
+                        user.append(item)
+                    else:
+                        user.append(self.unk)
+                if len(user) < 2:
+                    print('skip user')
+                    continue
+                num_users += 1
+                # create list of [sub-user: lst[item ids], target_item_id]
+                for i in range(1, len(user)):
+                    sub_user = user[:i]
+                    target_item = user[i]
+                    data.append(([self.item2idx[item] for item in sub_user], self.item2idx[target_item]))
 
         print("")
         pickle.dump(data, open(savepath, 'wb'))
@@ -108,7 +130,7 @@ def main():
     preprocess = Preprocess(unk=args.unk, data_dir=args.data_dir)
     preprocess.build(args.vocab, max_vocab=args.max_vocab)
     preprocess.convert(args.full_corpus, args.full_train_file)
-    preprocess.convert(args.test_corpus, args.test_file, test=True)
+    preprocess.process_test(args.test_corpus, args.test_file)
     if args.build_train_valid:
         preprocess.convert(args.train_corpus, args.train_file)
         preprocess.convert(args.valid_corpus, args.valid_file)
