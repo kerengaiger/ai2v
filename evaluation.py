9238eea38a110ea7c7e845601ce36d32bab6c78a
kerengaiger <keren.gaiger@gmail.com>
2021-05-19 16:06:18 +0300

    evaluation: change the default test set parameter

--- evaluation.py
+++ evaluation.py
@@ -9,7 +9,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--k', type=int, default=20, help="k for hr and mrr")
     parser.add_argument('--model', type=str, default='./output/i2v_best.pt', help="best model trained")
-    parser.add_argument('--test', type=str, default='./data/test_ai2v_batch_u.dat', help="test set for evaluation")
+    parser.add_argument('--test', type=str, default='./data/test_batch_u.dat', help="test set for evaluation")
     return parser.parse_args()
 
 
