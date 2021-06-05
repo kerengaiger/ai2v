6ab9c5a38c9ac3374b79d3d998bfb18d3a4234f4
kerengaiger <keren.gaiger@gmail.com>
2021-06-05 18:24:33 +0300

    evaluation: add out files for tracking hr and mrr for single samples of test set

--- evaluation.py
+++ evaluation.py
@@ -10,33 +10,47 @@ def parse_args():
     parser.add_argument('--k', type=int, default=20, help="k for hr and mrr")
     parser.add_argument('--model', type=str, default='./output/i2v_mix_batch__best.pt', help="best model trained")
     parser.add_argument('--test', type=str, default='./data/test.dat', help="test set for evaluation")
+    parser.add_argument('--hr_out', type=str, default='./output/hr_out.csv', help="hit at K for each test row")
+    parser.add_argument('--mrr_out', type=str, default='./output/mrr_out.csv', help="hit at K for each test row")
     return parser.parse_args()
 
 
-def mrr_k(model, eval_set, k):
+def mrr_k(model, eval_set, k, out_file):
     rec_rank = 0
 
     pbar = tqdm(eval_set)
 
-    for user_itemids, target_item in pbar:
-        items_ranked = model.inference(user_itemids).argsort()
-        top_k_items = items_ranked[-k:][::-1]
-        if target_item in top_k_items:
-            rec_rank += 1 / (np.where(top_k_items == target_item)[0][0] + 1)
+    with open(out_file, 'wb') as file:
+        for user_itemids, target_item in pbar:
+            items_ranked = model.inference(user_itemids).argsort()
+            top_k_items = items_ranked[-k:][::-1]
+            if target_item in top_k_items:
+                rec_rank += 1 / (np.where(top_k_items == target_item)[0][0] + 1)
+                file.write(f'{user_itemids}, {target_item}, {rec_rank}')
+                file.write('\n')
+            else:
+                file.write(f'{user_itemids}, {target_item}, 0')
+                file.write('\n')
     mrp_k = rec_rank / len(eval_set)
     return mrp_k
 
 
-def hr_k(model, eval_set, k):
+def hr_k(model, eval_set, k, out_file):
     in_top_k = 0
 
     pbar = tqdm(eval_set)
 
-    for user_itemids, target_item in pbar:
-        items_ranked = model.inference(user_itemids).argsort()
-        top_k_items = items_ranked[-k:][::-1]
-        if target_item in top_k_items:
-            in_top_k += 1
+    with open(out_file, 'wb') as file:
+        for user_itemids, target_item in pbar:
+            items_ranked = model.inference(user_itemids).argsort()
+            top_k_items = items_ranked[-k:][::-1]
+            if target_item in top_k_items:
+                in_top_k += 1
+                file.write(f'{user_itemids}, {target_item}, 1')
+                file.write('\n')
+            else:
+                file.write(f'{user_itemids}, {target_item}, 0')
+                file.write('\n')
     return in_top_k / len(eval_set)
 
 
@@ -44,8 +58,8 @@ def main():
     args = parse_args()
     model = t.load(args.model)
     eval_set = pickle.load(open(args.test, 'rb'))
-    print(f'hit ratio at {args.k}:', hr_k(model, eval_set, args.k))
-    print(f'mrr at {args.k}:', mrr_k(model, eval_set, args.k))
+    print(f'hit ratio at {args.k}:', hr_k(model, eval_set, args.k, args.hr_out))
+    print(f'mrr at {args.k}:', mrr_k(model, eval_set, args.k, args.mrr_out))
 
 
 if __name__ == '__main__':
