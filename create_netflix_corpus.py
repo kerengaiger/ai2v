f5328b10210243be764b7e6f5d669e219719fd59
kerengaiger <keren.gaiger@gmail.com>
2021-06-09 15:24:46 +0300

    create netflix full corpus file

---
+++ create_netflix_corpus.py
@@ -0,0 +1,17 @@
+import codecs
+import csv
+import os
+
+file_out = 'netflix_corpus.csv'
+data_dir = './archive'
+
+for file in os.listdir(data_dir):
+    with codecs.open(file_out, mode='w') as w_file:
+        writer = csv.writer(w_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        with codecs.open(file, 'r', encoding='utf-8') as r_file:
+            for line in r_file:
+                if ',' not in line:
+                    user = int(line.split(':')[0])
+                else:
+                    cus_id, rating, date = line.split(',')
+                    writer.writerow([user, cus_id, rating, date.replace('\n', '')])
