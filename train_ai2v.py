cbdcbf01f6f7f1fdb035b3afd0e1aeebbc5f7168
kerengaiger <keren.gaiger@gmail.com>
2021-06-09 12:37:17 +0300

    train_ai2v: fix the best cnfg

--- train_ai2v.py
+++ train_ai2v.py
@@ -142,9 +142,9 @@ def train_early_stop(cnfg, valid_users_path, pad_idx):
 
 
 def train(cnfg):
-    # cnfg['lr'] = 0.094871
-    # cnfg['e_dim'] = 20
-    # cnfg['n_negs'] = 7
+    cnfg['lr'] = 0.094871
+    cnfg['e_dim'] = 20
+    cnfg['n_negs'] = 7
     print(cnfg)
     idx2item = pickle.load(pathlib.Path(cnfg['data_dir'], 'idx2item.dat').open('rb'))
     item2idx = pickle.load(pathlib.Path(cnfg['data_dir'], 'item2idx.dat').open('rb'))
@@ -190,9 +190,9 @@ def train(cnfg):
 
 
 def train_evaluate(cnfg):
-    # cnfg['lr'] = 0.094871
-    # cnfg['e_dim'] = 20
-    # cnfg['n_negs'] = 7
+    cnfg['lr'] = 0.094871
+    cnfg['e_dim'] = 20
+    cnfg['n_negs'] = 7
     print(cnfg)
     valid_users_path = pathlib.Path(cnfg['data_dir'], cnfg['valid'])
     item2idx = pickle.load(pathlib.Path(cnfg['data_dir'], 'item2idx.dat').open('rb'))
