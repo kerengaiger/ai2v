e0a12f046f959c5c98fb88302b5a9f3f3a5e8984
kerengaiger <keren.gaiger@gmail.com>
2021-06-10 09:32:07 +0300

    create_netflix_corpus: fix the creation

--- create_netflix_corpus.py
+++ create_netflix_corpus.py
@@ -2,16 +2,18 @@ import codecs
 import csv
 import os
 
-file_out = 'netflix_corpus.csv'
+file_out = './data/netflix_corpus.csv'
 data_dir = './archive'
 
-for file in os.listdir(data_dir):
-    with codecs.open(file_out, mode='w') as w_file:
-        writer = csv.writer(w_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        with codecs.open(os.path.join(data_dir, file), 'r', encoding='utf-8') as r_file:
+with codecs.open(file_out, mode='w') as w_file:
+    writer = csv.writer(w_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for file in os.listdir(data_dir):
+        print(file)
+        with codecs.open(os.path.join(data_dir, file), 'rU') as r_file:
             for line in r_file:
                 if ',' not in line:
                     user = int(line.split(':')[0])
+
                 else:
                     cus_id, rating, date = line.split(',')
-                    writer.writerow([user, cus_id, rating, date.replace('\n', '')])
+                    writer.writerow([user, cus_id, rating, date])
