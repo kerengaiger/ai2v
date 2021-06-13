7d293ba7bdcc749d4c4108ce55a54780bf12b1da
kerengaiger <keren.gaiger@gmail.com>
2021-06-13 23:02:57 +0300

    train_ai2v: add the T

--- train_ai2v.py
+++ train_ai2v.py
@@ -50,7 +50,7 @@ def run_epoch(train_dl, epoch, sgns, optim, pad_idx):
     for batch_titems, batch_citems in pbar:
         if next(sgns.parameters()).is_cuda:
             batch_titems, batch_citems = batch_titems.cuda(), batch_citems.cuda()
-            batch_pad_ids = (batch_citems == pad_idx).nonzero()
+            batch_pad_ids = (batch_citems == pad_idx).nonzero().T
             batch_pad_ids = batch_pad_ids.cuda()
         else:
             batch_pad_ids = (batch_citems == pad_idx).nonzero().T
