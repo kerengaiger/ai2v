49917d8f67cc1f6ca5c6c6537e4365d436659398
kerengaiger <keren.gaiger@gmail.com>
2021-06-19 08:58:42 +0300

    train_i2v: model_save and load with a change in model name

--- train_i2v.py
+++ train_i2v.py
@@ -78,7 +78,7 @@ def train(cnfg):
     for epoch in range(1, cnfg['max_epoch'] + 1):
         train_loss, sgns = run_epoch(train_loader, epoch, sgns, optim)
 
-    save_model(cnfg, model, sgns, '_mix_batch_')
+    save_model(cnfg, model, sgns)
 
     # Evaluate on test set
     log_dir = cnfg['log_dir'] + '/' + str(datetime.datetime.now().timestamp())
@@ -144,7 +144,7 @@ def train_early_stop(cnfg, valid_users_path, pad_idx):
         if valid_loss < valid_losses[-1]:
             patience_count = 0
             best_epoch = epoch
-            save_model(cnfg, model, sgns, '_mix_batch_')
+            save_model(cnfg, model, sgns)
 
         else:
             patience_count += 1
@@ -167,7 +167,7 @@ def train_evaluate(cnfg):
 
     best_epoch = train_early_stop(cnfg, valid_users_path, item2idx['pad'])
 
-    best_model = t.load(pathlib.Path(cnfg['save_dir'], cnfg['model'] + '_mix_batch_' + '_best.pt'))
+    best_model = t.load(pathlib.Path(cnfg['save_dir'], cnfg['model'] + f'_{cnfg["ds_name"]}.pt'))
 
     valid_loss = calc_loss_on_set(best_model, valid_users_path, item2idx['pad'], cnfg['mini_batch'], cnfg['window_size'])
     return {'valid_loss': (valid_loss, 0.0), 'early_stop_epoch': (best_epoch, 0.0)}
