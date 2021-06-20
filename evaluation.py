663c0ebd389cad4edc5236511d93d69a20250155
kerengaiger <keren.gaiger@gmail.com>
2021-06-20 10:43:16 +0300

    evaluation: back to the old config of evaluation , for now duw to memory leakage

--- evaluation.py
+++ evaluation.py
@@ -12,7 +12,6 @@ from scipy.stats import ttest_ind
 from train_utils import UserBatchIncrementDataset
 
 
-
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--k', type=int, default=20, help="k for hr and mrr")
@@ -26,77 +25,81 @@ def parse_args():
     return parser.parse_args()
 
 
-def mrr_k(model, test_path, k, out_file, pad_idx, batch_size, window_size):
-    test_dataset = UserBatchIncrementDataset(test_path, pad_idx, window_size)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=16)
-    pbar = tqdm(test_loader)
+# def mrr_k(model, test_path, k, out_file, pad_idx, batch_size, window_size):
+#     test_dataset = UserBatchIncrementDataset(test_path, pad_idx, window_size)
+#     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=16)
+#     pbar = tqdm(test_loader)
+#
+#     rec_rank = 0
+#     for batch_titems, batch_citems in pbar:
+#         all_titems = t.tensor(np.repeat(np.array([range(model.ai2v.tvectors.weight.size()[0])]), batch_size, axis=0))
+#         if next(model.parameters()).is_cuda:
+#             batch_citems, batch_titems = batch_citems.cuda(), batch_titems.cuda()
+#             all_titems = all_titems.cuda()
+#         mask_pad_ids = (batch_citems == pad_idx)
+#         batch_sub_users = model.ai2v(all_titems, batch_citems, mask_pad_ids)
+#         all_tvecs = model.ai2v.Bt(model.ai2v.forward_t(all_titems))
+#         sim = model.similarity(batch_sub_users, all_tvecs, all_titems).squeeze()
+#         items_ranked = t.argsort(sim, descending=True)[:, :k]
+#         rec_rank += 1 / (items_ranked.eq(batch_titems.view(-1, 1)).nonzero()[:, 1] + 1)
+#
+#     return rec_rank / len(pbar)
 
+def mrr_k(model, eval_set, k, out_file):
     rec_rank = 0
-    for batch_titmes, batch_citmes in pbar:
-        all_titems = t.tensor(np.repeat(np.array([range(model.ai2v.tvectors.weight.size()[0])]), batch_size, axis=0))
-        if next(model.parameters()).is_cuda:
-            batch_citems, batch_titmes = batch_citems.cuda(), batch_titmes.cuda()
-            all_titems = all_titems.cuda()
-        mask_pad_ids = (batch_citems == pad_idx)
-        batch_sub_users = model.ai2v(all_titems, batch_citems, mask_pad_ids)
-        all_tvecs = model.ai2v.Bt(model.ai2v.forward_t(all_titems))
-        sim = model.similarity(batch_sub_users, all_tvecs, all_titems).squeeze()
-        items_ranked = t.argsort(sim, descending=True)[:, :k]
-        rec_rank += 1 / (items_ranked.nonzero()[:,1] +1)
 
-    return rec_rank / len(pbar)
-    # rec_rank = 0
-    #
-    # pbar = tqdm(eval_set)
-    #
-    # with open(out_file, 'w') as file:
-    #     for i, (user_itemids, target_item) in enumerate(pbar):
-    #         items_ranked = model.inference(user_itemids).argsort()
-    #         top_k_items = items_ranked[-k:][::-1]
-    #         if target_item in top_k_items:
-    #             rec_rank += 1 / (np.where(top_k_items == target_item)[0][0] + 1)
-    #             file.write(f'{str(i)}, {target_item}, {rec_rank}')
-    #             file.write('\n')
-    #         else:
-    #             file.write(f'{str(i)}, {target_item}, 0')
-    #             file.write('\n')
-    # mrp_k = rec_rank / len(eval_set)
-    # return mrp_k
+    pbar = tqdm(eval_set)
+
+    with open(out_file, 'w') as file:
+        for i, (user_itemids, target_item) in enumerate(pbar):
+            items_ranked = model.inference(user_itemids).argsort()
+            top_k_items = items_ranked[-k:][::-1]
+            if target_item in top_k_items:
+                rec_rank += 1 / (np.where(top_k_items == target_item)[0][0] + 1)
+                file.write(f'{str(i)}, {target_item}, {rec_rank}')
+                file.write('\n')
+            else:
+                file.write(f'{str(i)}, {target_item}, 0')
+                file.write('\n')
+    mrp_k = rec_rank / len(eval_set)
+    return mrp_k
 
 
-def hr_k(model, test_path, k, out_file, pad_idx, batch_size, window_size):
-    test_dataset = UserBatchIncrementDataset(test_path, pad_idx, window_size)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=16)
-    pbar = tqdm(test_loader)
+# def hr_k(model, test_path, k, out_file, pad_idx, batch_size, window_size):
+#     test_dataset = UserBatchIncrementDataset(test_path, pad_idx, window_size)
+#     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=16)
+#     pbar = tqdm(test_loader)
+#
+#     in_top_k = 0
+#     for batch_titems, batch_citems in pbar:
+#         all_titems = t.tensor(np.repeat(np.array([range(model.ai2v.tvectors.weight.size()[0])]), batch_size, axis=0))
+#         if next(model.parameters()).is_cuda:
+#             batch_citems, batch_titems = batch_citems.cuda(), batch_titems.cuda()
+#             all_titems = all_titems.cuda()
+#         mask_pad_ids = (batch_citems == pad_idx)
+#         batch_sub_users = model.ai2v(all_titems, batch_citems, mask_pad_ids)
+#         all_tvecs = model.ai2v.Bt(model.ai2v.forward_t(all_titems))
+#         sim = model.similarity(batch_sub_users, all_tvecs, all_titems).squeeze()
+#         items_ranked = t.argsort(sim, descending=True)[:, :k]
+#         in_top_k += items_ranked.eq(batch_titems.view(-1, 1)).sum()
+#
+#     return in_top_k / len(pbar)
 
+def hr_k(model, eval_set, k, out_file):
+    pbar = tqdm(eval_set)
     in_top_k = 0
-    for batch_titmes, batch_citems in pbar:
-        all_titems = t.tensor(np.repeat(np.array([range(model.ai2v.tvectors.weight.size()[0])]), batch_size, axis=0))
-        if next(model.parameters()).is_cuda:
-            batch_citems, batch_titmes = batch_citems.cuda(), batch_titmes.cuda()
-            all_titems = all_titems.cuda()
-        mask_pad_ids = (batch_citems == pad_idx)
-        batch_sub_users = model.ai2v(all_titems, batch_citems, mask_pad_ids)
-        all_tvecs = model.ai2v.Bt(model.ai2v.forward_t(all_titems))
-        sim = model.similarity(batch_sub_users, all_tvecs, all_titems).squeeze()
-        items_ranked = t.argsort(sim, descending=True)[:, :k]
-        in_top_k += items_ranked.eq(batch_titmes.view(-1, 1)).sum()
-
-    return in_top_k / len(pbar)
-
-    # pbar = tqdm(eval_set)
-    # with open(out_file, 'w') as file:
-    #     for i, (user_itemids, target_item) in enumerate(pbar):
-    #         items_ranked = model.inference(user_itemids).argsort()
-    #         top_k_items = items_ranked[-k:][::-1]
-    #         if target_item in top_k_items:
-    #             in_top_k += 1
-    #             file.write(f'{str(i)}, {target_item}, 1')
-    #             file.write('\n')
-    #         else:
-    #             file.write(f'{str(i)}, {target_item}, 0')
-    #             file.write('\n')
-    # return in_top_k / len(eval_set)
+    with open(out_file, 'w') as file:
+        for i, (user_itemids, target_item) in enumerate(pbar):
+            items_ranked = model.inference(user_itemids).argsort()
+            top_k_items = items_ranked[-k:][::-1]
+            if target_item in top_k_items:
+                in_top_k += 1
+                file.write(f'{str(i)}, {target_item}, 1')
+                file.write('\n')
+            else:
+                file.write(f'{str(i)}, {target_item}, 0')
+                file.write('\n')
+    return in_top_k / len(eval_set)
 
 
 def test_p_value(ai2v_file, i2v_file):
@@ -114,12 +117,14 @@ def main():
     args = parse_args()
     model = t.load(args.model)
     model = t.nn.DataParallel(model)
-    # eval_set = pickle.load(open(args.test, 'rb'))
+    eval_set = pickle.load(open(args.test, 'rb'))
     item2idx = pickle.load(pathlib.Path(args.data_dir, 'item2idx.dat').open('rb'))
-    print(f'hit ratio at {args.k}:', hr_k(model.module, pathlib.Path(args.data_dir, args.test), args.k, args.hr_out,
-                                          item2idx['pad'], args.batch_size, args.window_size))
-    print(f'mrr at {args.k}:', mrr_k(model.module, pathlib.Path(args.data_dir, args.test), args.k, args.mrr_out,
-                                     item2idx['pad'], args.batch_size, args.window_size))
+    # print(f'hit ratio at {args.k}:', hr_k(model.module, pathlib.Path(args.data_dir, args.test), args.k, args.hr_out,
+    #                                       item2idx['pad'], args.batch_size, args.window_size))
+    print(f'hit ratio at {args.k}:', hr_k(model.module, eval_set, args.k, args.hr_out))
+    # print(f'mrr at {args.k}:', mrr_k(model.module, pathlib.Path(args.data_dir, args.test), args.k, args.mrr_out,
+    #                                  item2idx['pad'], args.batch_size, args.window_size))
+    print(f'mrr at {args.k}:', mrr_k(model.module, eval_set, args.k, args.mrr_out))
 
 
 if __name__ == '__main__':
