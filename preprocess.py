54a068f7449fd8461d38b68c4633927a9ee880e6
kerengaiger <keren.gaiger@gmail.com>
2021-07-14 10:34:13 +0300

    preprocess: add split_strategy and adjust convert function

--- preprocess.py
+++ preprocess.py
@@ -11,15 +11,23 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='./data/', help="data directory path")
     parser.add_argument('--vocab', type=str, default='./data/corpus_avi.txt', help="corpus path for building vocab")
+    parser.add_argument('--split_strategy', type=str, choices=['leave_one_out', 'users_split'],
+                        help="way of splitting to train and test")
     parser.add_argument('--full_corpus', type=str, default='./data/corpus_avi.txt', help="corpus path")
     parser.add_argument('--test_corpus', type=str, default='./data/test_corpus_avi.txt', help="test corpus path")
-    parser.add_argument('--build_train_valid', action='store_true', help="build part train and validation sets from provided paths")
-    parser.add_argument('--train_corpus', type=str, default='./data/train_corpus_avi.txt', help="part train corpus path to build part train set, in case build_train_valid is True")
-    parser.add_argument('--valid_corpus', type=str, default='./data/valid_corpus_avi.txt', help="validation corpys path to build validation set, in case build_train_valid is True")
+    parser.add_argument('--build_train_valid', action='store_true',
+                        help="build part train and validation sets from provided paths")
+    parser.add_argument('--train_corpus', type=str, default='./data/train_corpus_avi.txt',
+                        help="part train corpus path to build part train set, in case build_train_valid is True")
+    parser.add_argument('--valid_corpus', type=str, default='./data/valid_corpus_avi.txt',
+                        help="validation corpys path to build validation set, in case build_train_valid is True")
     parser.add_argument('--full_train_file', type=str, default='./data/full_train.dat', help="full train file name")
-    parser.add_argument('--train_file', type=str, default='./data/train.dat', help="train file name, in case of build_valid")
-    parser.add_argument('--valid_file', type=str, default='./data/valid.dat', help="validation file name, in case of build_valid")
-    parser.add_argument('--test_file', type=str, default='./data/test.dat', help="test file of sub users and target items")
+    parser.add_argument('--train_file', type=str, default='./data/train.dat',
+                        help="train file name, in case of build_valid")
+    parser.add_argument('--valid_file', type=str, default='./data/valid.dat',
+                        help="validation file name, in case of build_valid")
+    parser.add_argument('--test_file', type=str, default='./data/test.dat',
+                        help="test file of sub users and target items")
     parser.add_argument('--unk', type=str, default='<UNK>', help="UNK token")
     parser.add_argument('--max_vocab', type=int, default=20000, help="maximum number of vocab")
     parser.add_argument('--max_user', type=int,default=1000, help='maximum length of usr')
@@ -62,20 +70,16 @@ class Preprocess(object):
         pickle.dump(self.item2idx, open(os.path.join(self.data_dir, 'item2idx.dat'), 'wb'))
         print("build done")
 
-    def create_train_samp(self, user, item_target, pad_idx, max_user):
+    def create_train_samp(self, user, item_target):
         sub_user = user[:item_target]
-        # pad_times = max_user - len(sub_user)
-        # sub_user += pad_times * ['pad']
         target_item = user[item_target]
         return [self.item2idx[item] for item in sub_user], self.item2idx[target_item]
 
-    def convert(self, filepath, savepath, max_user):
+    def convert(self, filepath, savepath, max_user, split_strategy='users_split'):
         print("converting corpus...")
         step = 0
         data = []
         usrs_len = []
-        item2idx = pickle.load(pathlib.Path(self.data_dir, 'item2idx.dat').open('rb'))
-        pad_idx = item2idx['pad']
         with codecs.open(filepath, 'r', encoding='utf-8') as file:
             num_users = 0
             for line in file:
@@ -100,9 +104,12 @@ class Preprocess(object):
                 sub_users = [user[x:x+max_user] for x in range(0, len(user), max_user)]
                 for user in sub_users:
                     for item_target in range(1, len(user)):
-                        data.append((self.create_train_samp(user, item_target, pad_idx, max_user)))
+                        data.append((self.create_train_samp(user, item_target)))
 
         print("")
+        if split_strategy == 'leave_one_out':
+            # send leave_one_out when converting valid and test, then take only the last (sub-user, item)
+            data = data[-1]
         pickle.dump(data, open(savepath, 'wb'))
         print("conversion done")
         print("num of users:", num_users)
@@ -114,10 +121,10 @@ def main():
     preprocess = Preprocess(unk=args.unk, data_dir=args.data_dir)
     preprocess.build(args.vocab, max_vocab=args.max_vocab)
     preprocess.convert(args.full_corpus, args.full_train_file, args.max_user)
-    preprocess.convert(args.test_corpus, args.test_file, args.max_user)
+    preprocess.convert(args.test_corpus, args.test_file, args.max_user, split_strategy=args.split_strategy)
     if args.build_train_valid:
         preprocess.convert(args.train_corpus, args.train_file, args.max_user)
-        preprocess.convert(args.valid_corpus, args.valid_file, args.max_user)
+        preprocess.convert(args.valid_corpus, args.valid_file, args.max_user, split_strategy=args.split_strategy)
 
 
 if __name__ == '__main__':
