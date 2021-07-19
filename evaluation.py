ba3eefda58e460ce6936393b015999b7f9640160
shirtsipory <shirtsipory@gmail.com>
2021-07-19 10:52:03 +0300

    Adding MPR to evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -21,6 +21,7 @@ def parse_args():
     parser.add_argument('--rank_out', type=str, default='rank_out.csv', help="ranked list of items for test users")
     parser.add_argument('--hr_out', type=str, default='hr_out.csv', help="hit at K for each test row")
     parser.add_argument('--mrr_out', type=str, default='mrr_out.csv', help="hit at K for each test row")
+    parser.add_argument('--mpr_out', type=str, default='mpr_out.csv', help="percentile for each test row")
     return parser.parse_args()
 
 
@@ -64,6 +65,24 @@ def hr_k(model, eval_set, k, out_file, rank_out_file):
     return in_top_k / len(eval_set)
 
 
+def mpr(model, eval_set, out_file):
+    rec_percent = 0
+    pbar = tqdm(eval_set)
+    with open(out_file, 'w') as file:
+        for i, (user_itemids, target_item) in enumerate(pbar):
+            items_ranked = model.inference(user_itemids).argsort()
+            items = items_ranked[:][::-1]
+            if target_item in items:
+                rec_percent += (float(np.where(items == target_item)[0][0]) / len(items))
+                file.write(f'{str(i)}, {target_item}, {rec_percent}')
+                file.write('\n')
+            else:
+                file.write(f'{str(i)}, {target_item}, 0')
+                file.write('\n')
+    mpr = rec_percent / float(len(eval_set))
+    return mpr
+
+
 def test_p_value(ai2v_file, i2v_file):
     '''
     :param ai2v_file: csv file containing u_id, item_id and the result of the tested metric, applied on ai2v model
@@ -83,6 +102,7 @@ def main():
     print(f'hit ratio at {args.k}:', hr_k(model.module, eval_set, args.k, os.path.join(args.output_dir, args.hr_out),
                                           os.path.join(args.output_dir, args.rank_out)))
     print(f'mrr at {args.k}:', mrr_k(model.module, eval_set, args.k, os.path.join(args.output_dir, args.mrr_out)))
+    print(f'mpr:', mpr(model.module, eval_set, os.path.join(args.output_dir, args.mpr_out)))
 
 
 if __name__ == '__main__':
