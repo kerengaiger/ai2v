c11cc4778aede5b71cb92d5dc30fbfc6e16fb33c
kerengaiger <keren.gaiger@gmail.com>
2021-08-15 14:06:01 +0300

    ai2v_model: add debug print

--- ai2v_model.py
+++ ai2v_model.py
@@ -209,6 +209,7 @@ class SGNS(nn.Module):
         batch_titems = t.cat([batch_titems.reshape(-1, 1), batch_nitems], 1)
         # get embeddings of context items after self attention
         batch_sa_citems = self.sasrec(batch_citems)
+        print(batch_sa_citems.shape)
         batch_sub_users = self.ai2v(batch_titems, batch_sa_citems, mask_pad_ids)
         batch_tvecs = self.ai2v.Bt(self.ai2v.forward_t(batch_titems))
         if [param for param in self.ai2v.parameters()][0].is_cuda:
