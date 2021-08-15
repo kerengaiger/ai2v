c1647c3f470782c83724075d35a6b11870b78d54
kerengaiger <keren.gaiger@gmail.com>
2021-08-15 14:23:26 +0300

    add debug prints to check cuda compat

--- train_ai2v.py
+++ train_ai2v.py
@@ -97,7 +97,16 @@ def train_early_stop(cnfg, valid_users_path, pad_idx):
                     num_heads=cnfg['num_heads'])
     sgns = SGNS(sasrec=sasrec, ai2v=model, vocab_size=vocab_size, n_negs=cnfg['n_negs'], weights=weights)
 
+    print(model)
+    print(next(model.parameters()).is_cuda)
+    print(sasrec)
+    print(next(sasrec.parameters()).is_cuda)
+    print(sgns)
+    print(next(sgns.parameters()).is_cuda)
     sgns.to(device)
+    print(next(model.parameters()).is_cuda)
+    print(next(sasrec.parameters()).is_cuda)
+    print(next(sgns.parameters()).is_cuda)
 
     optim = Adagrad(sgns.parameters(), lr=cnfg['lr'])
     scheduler = lr_scheduler.MultiStepLR(optim, milestones=[2, 4, 5, 6, 7, 8, 10, 12, 14, 16], gamma=0.5)
