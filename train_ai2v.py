f379ba541f95b3684d812a11ae474be14becf6ac
kerengaiger <keren.gaiger@gmail.com>
2021-08-21 12:37:40 +0300

    train_ai2v: add num_workers and merge between train and train_evaluate

--- train_ai2v.py
+++ train_ai2v.py
@@ -6,7 +6,7 @@ import pickle
 
 import numpy as np
 import torch as t
-from torch.optim import Adagrad, lr_scheduler, AdamW, Adam
+from torch.optim import Adagrad, lr_scheduler
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
@@ -15,7 +15,6 @@ from ai2v_model import AttentiveItemToVec
 from ai2v_model import SGNS
 
 from train_utils import save_model, configure_weights, UserBatchIncrementDataset
-from evaluation import hr_k, mrr_k
 
 import argparse
 
@@ -31,6 +30,7 @@ def parse_args():
     parser.add_argument('--max_batch_size', type=int, default=200, help="max number of training obs in batch")
     parser.add_argument('--log_dir', type=str, default='tensorboard/logs/mylogdir', help="logs dir for tensorboard")
     parser.add_argument('--k', type=int, default=20, help="k to calc hrr_k and mrr_k evaluation metrics")
+    parser.add_argument('--num_workers', type=int, default=8, help="num workers to load train_loader")
     parser.add_argument('--hr_out', type=str, default='hr.csv', help="hit at K for each test row")
     parser.add_argument('--rr_out', type=str, default='mrr.csv', help="hit at K for each test row")
     parser.add_argument('--best_cnfg', type=str, default='best_cnfg.pkl', help="best cnfg of hyper params")
@@ -63,9 +63,9 @@ def run_epoch(train_dl, epoch, sgns, optim, pad_idx):
     return train_loss, sgns
 
 
-def calc_loss_on_set(sgns, valid_users_path, pad_idx, batch_size, window_size):
+def calc_loss_on_set(sgns, valid_users_path, pad_idx, batch_size, window_size, num_workers):
     dataset = UserBatchIncrementDataset(valid_users_path, pad_idx, window_size)
-    valid_dl = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
+    valid_dl = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
 
     pbar = tqdm(valid_dl)
     valid_losses = []
@@ -81,13 +81,14 @@ def calc_loss_on_set(sgns, valid_users_path, pad_idx, batch_size, window_size):
     return np.array(valid_losses).mean()
 
 
-def train_early_stop(cnfg, valid_users_path, pad_idx):
+def train(cnfg, valid_users_path=None):
     idx2item = pickle.load(pathlib.Path(cnfg['data_dir'], cnfg['idx2item']).open('rb'))
+    item2idx = pickle.load(pathlib.Path(cnfg['data_dir'], cnfg['item2idx']).open('rb'))
 
     weights = configure_weights(cnfg, idx2item)
     vocab_size = len(idx2item)
 
-    model = AttentiveItemToVec(padding_idx=pad_idx, vocab_size=vocab_size, embedding_size=cnfg['e_dim'])
+    model = AttentiveItemToVec(padding_idx=item2idx['pad'], vocab_size=vocab_size, embedding_size=cnfg['e_dim'])
     sgns = SGNS(ai2v=model, vocab_size=vocab_size, n_negs=cnfg['n_negs'], weights=weights)
 
     if cnfg['cuda']:
@@ -104,30 +105,37 @@ def train_early_stop(cnfg, valid_users_path, pad_idx):
     patience_count = 0
     t.autograd.set_detect_anomaly(True)
 
-    dataset = UserBatchIncrementDataset(pathlib.Path(cnfg['data_dir'], cnfg['train']), pad_idx, cnfg['window_size'])
-    train_loader = DataLoader(dataset, batch_size=cnfg['mini_batch'], shuffle=True, num_workers=16, pin_memory=True)
+    dataset = UserBatchIncrementDataset(pathlib.Path(cnfg['data_dir'], cnfg['train']), item2idx['pad'],
+                                        cnfg['window_size'])
 
     for epoch in range(1, cnfg['max_epoch'] + 1):
-        train_loss, sgns = run_epoch(train_loader, epoch, sgns, optim, pad_idx)
+        train_loader = DataLoader(dataset, batch_size=cnfg['mini_batch'], shuffle=True, num_workers=cnfg['num_workers'],
+                                  pin_memory=True)
+        train_loss, sgns = run_epoch(train_loader, epoch, sgns, optim, item2idx['pad'])
         writer.add_scalar("Loss/train", train_loss, epoch)
 
-        valid_loss = calc_loss_on_set(sgns, valid_users_path, pad_idx, cnfg['mini_batch'], cnfg['window_size'])
-        writer.add_scalar("Loss/validation", valid_loss, epoch)
-        print(f'valid loss:{valid_loss}')
+        if valid_users_path is not None:
+            valid_loss = calc_loss_on_set(sgns, valid_users_path, item2idx['pad'], cnfg['mini_batch'],
+                                          cnfg['window_size'], cnfg['num_workers'])
+            writer.add_scalar("Loss/validation", valid_loss, epoch)
+            print(f'valid loss:{valid_loss}')
 
-        if valid_loss < valid_losses[-1]:
-            patience_count = 0
-            best_epoch = epoch
-            save_model(cnfg, model, sgns)
+            if valid_loss < valid_losses[-1]:
+                patience_count = 0
+                best_epoch = epoch
+                save_model(cnfg, model, sgns)
 
+            else:
+                patience_count += 1
+                if patience_count == cnfg['patience']:
+                    print(f"Early stopping")
+                    break
+
+            valid_losses.append(valid_loss)
+            scheduler.step()
         else:
-            patience_count += 1
-            if patience_count == cnfg['patience']:
-                print(f"Early stopping")
-                break
-
-        valid_losses.append(valid_loss)
-        scheduler.step()
+            # Save model in each iteration in case we are not in early_stop mode
+            save_model(cnfg, model, sgns)
 
     writer.flush()
     writer.close()
@@ -135,66 +143,17 @@ def train_early_stop(cnfg, valid_users_path, pad_idx):
     return best_epoch
 
 
-def train(cnfg):
-    # cnfg['lr'] = 0.094871
-    # cnfg['e_dim'] = 20
-    # cnfg['n_negs'] = 7
-    # cnfg['mini_batch'] = 32
-    print(cnfg)
-    idx2item = pickle.load(pathlib.Path(cnfg['data_dir'], cnfg['idx2item']).open('rb'))
-    item2idx = pickle.load(pathlib.Path(cnfg['data_dir'], cnfg['item2idx']).open('rb'))
-
-    weights = configure_weights(cnfg, idx2item)
-    vocab_size = len(idx2item)
-
-    model = AttentiveItemToVec(padding_idx=item2idx['pad'], vocab_size=vocab_size, embedding_size=cnfg['e_dim'])
-    sgns = SGNS(ai2v=model, vocab_size=vocab_size, n_negs=cnfg['n_negs'], weights=weights)
-    dataset = UserBatchIncrementDataset(pathlib.Path(cnfg['data_dir'], cnfg['train']), item2idx['pad'],
-                                        cnfg['window_size'])
-    train_loader = DataLoader(dataset, batch_size=cnfg['mini_batch'], shuffle=True, pin_memory=True, num_workers=16)
-
-    if cnfg['cuda']:
-        sgns = sgns.cuda()
-
-    optim = Adagrad(sgns.parameters(), lr=cnfg['lr'])
-    scheduler = lr_scheduler.MultiStepLR(optim, milestones=[2, 4, 6, 8, 10, 12, 14, 16], gamma=0.5)
-
-    for epoch in range(1, cnfg['max_epoch'] + 1):
-        train_loss, sgns = run_epoch(train_loader, epoch, sgns, optim, item2idx['pad'])
-        scheduler.step()
-
-    save_model(cnfg, model, sgns)
-
-    # Evaluate on test set
-    # log_dir = cnfg['log_dir'] + '/' + str(datetime.datetime.now().timestamp())
-    # writer = SummaryWriter(log_dir=log_dir)
-    #
-    # eval_set = pickle.load(pathlib.Path(cnfg['data_dir'], cnfg['test']).open('rb'))
-    # k = cnfg['k']
-    #
-    # writer.add_hparams(hparam_dict=cnfg,
-    #                    metric_dict={f'hit_ratio_{k}': hr_k(sgns, eval_set, k, cnfg['hr_out']),
-    #                                 f'mrr_{k}': mrr_k(sgns, eval_set, k, cnfg['rr_out'])},
-    #                    run_name='ai2v_user_batch')
-
-    # writer.flush()
-    # writer.close()
-
-
 def train_evaluate(cnfg):
-    # cnfg['lr'] = 0.094871
-    # cnfg['e_dim'] = 20
-    # cnfg['n_negs'] = 7
-    # cnfg['mini_batch'] = 32
     print(cnfg)
     valid_users_path = pathlib.Path(cnfg['data_dir'], cnfg['valid'])
     item2idx = pickle.load(pathlib.Path(cnfg['data_dir'], cnfg['item2idx']).open('rb'))
 
-    best_epoch = train_early_stop(cnfg, valid_users_path, item2idx['pad'])
+    best_epoch = train(cnfg, valid_users_path)
 
     best_model = t.load(pathlib.Path(cnfg['save_dir'], 'model.pt'))
 
-    valid_loss = calc_loss_on_set(best_model, valid_users_path, item2idx['pad'], cnfg['mini_batch'], cnfg['window_size'])
+    valid_loss = calc_loss_on_set(best_model, valid_users_path, item2idx['pad'], cnfg['mini_batch'],
+                                  cnfg['window_size'], cnfg['num_workers'])
     return {'valid_loss': (valid_loss, 0.0), 'early_stop_epoch': (best_epoch, 0.0)}
 
 
