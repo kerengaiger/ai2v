f456f5e3d28fe23b58bf91e3078bb32a5f449372
kerengaiger <keren.gaiger@gmail.com>
2021-09-04 17:23:54 +0300

    models.__init__: add model keys for configuration of SGNS

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,7 @@ from models.ai2v_model import AttentiveItemToVec as ai2v
 from models.ai2v_model import SGNS as sgns_ai2v
 from models.i2v_model import Item2Vec as i2v
 from models.i2v_model import SGNS as sgns_i2v
+
+ai2v_cnfg_keys = ['padding_idx', 'vocab_size', 'embedding_size']
+i2v_cnfg_keys = ['padding_idx', 'vocab_size', 'embedding_size', 'num_heads',
+                 'num_blocks', 'dropout_rate']
