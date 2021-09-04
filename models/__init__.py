6a8382ed28ca4f6847d9606bc5bfff9935831258
kerengaiger <keren.gaiger@gmail.com>
2021-09-04 11:45:24 +0300

    __init__: add init of models

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from models.ai2v_model import AttentiveItemToVec
+from models.ai2v_model import SGNS as sgns_ai2v
+from models.i2v_model import Item2Vec
+from models.i2v_model import SGNS as sgns_i2v
