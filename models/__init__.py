7304c0be5720ae4b04ece22f5282bbbc75501763
kerengaiger <keren.gaiger@gmail.com>
2021-09-04 16:01:59 +0300

    __init__: name classes differently

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from models.ai2v_model import AttentiveItemToVec
+from models.ai2v_model import AttentiveItemToVec as ai2v
 from models.ai2v_model import SGNS as sgns_ai2v
-from models.i2v_model import Item2Vec
+from models.i2v_model import Item2Vec as i2v
 from models.i2v_model import SGNS as sgns_i2v
