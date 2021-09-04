8cf9820677e2a44dc42d646e7c0e3da021379b7a
kerengaiger <keren.gaiger@gmail.com>
2021-09-04 18:30:09 +0300

    __init__: typo

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,6 @@ from models.ai2v_model import SGNS as sgns_ai2v
 from models.i2v_model import Item2Vec as i2v
 from models.i2v_model import SGNS as sgns_i2v
 
-ai2v_cnfg_keys = ['padding_idx', 'vocab_size', 'e_dim']
-i2v_cnfg_keys = ['padding_idx', 'vocab_size', 'e_dim', 'num_heads',
+i2v_cnfg_keys = ['padding_idx', 'vocab_size', 'e_dim']
+ai2v_cnfg_keys = ['padding_idx', 'vocab_size', 'e_dim', 'num_heads',
                  'num_blocks', 'dropout_rate']
