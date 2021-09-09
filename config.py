76e24b8942a2d56ad96f8727f9575fcd7fd58817
kerengaiger <keren.gaiger@gmail.com>
2021-09-09 15:33:47 +0300

    remove unnecessary python files

--- config.py
+++
@@ -1,9 +0,0 @@
-DATA_DIR = './data/'
-SAVE_DIR = './pts/'
-
-DATA_FPATH = 'ml-100k/u.data'
-DATA_COLS = ['user_id', 'item_id', 'rating', 'timestamp']
-POS_THRESH = 3.5
-
-TRAIN_PATH = DATA_DIR + 'train.dat'
-VALID_PATH = DATA_DIR + 'valid.txt'
