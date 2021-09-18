c693889998a0f78c74cd27e06851adfdcb1c7582
kerengaiger <keren.gaiger@gmail.com>
2021-09-18 14:05:59 +0300

    ai2v_model: use old ai2v_model version

--- models/ai2v_model.py
+++ models/ai2v_model.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import datetime
-from tqdm import tqdm
 import numpy as np
 import torch as t
 import torch.nn as nn
@@ -10,64 +8,76 @@ from torch import FloatTensor as FT
 
 
 class AttentiveItemToVec(nn.Module):
-    def __init__(self, padding_idx, vocab_size, e_dim, num_heads,
-                 num_blocks, dropout_rate):
+    def __init__(self, padding_idx, vocab_size, embedding_size, window_size, d_alpha=60, N=1):
         super(AttentiveItemToVec, self).__init__()
         self.name = 'ai2v'
         self.vocab_size = vocab_size
-        self.e_dim = e_dim
+        self.embedding_size = embedding_size
+        self.d_alpha = d_alpha
         self.pad_idx = padding_idx
-        self.num_heads = num_heads
-        self.num_blocks = num_blocks
-        self.tvectors = nn.Embedding(self.vocab_size, self.e_dim, padding_idx=padding_idx)
-        self.cvectors = nn.Embedding(self.vocab_size, self.e_dim, padding_idx=padding_idx)
-        self.last_item_vectors = nn.Embedding(self.vocab_size, self.e_dim, padding_idx=padding_idx)
+        self.window_size = window_size
+        self.tvectors = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=padding_idx)
+        self.cvectors = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=padding_idx)
         self.tvectors.weight = nn.Parameter(t.cat([FT(self.vocab_size - 1,
-                                                      self.e_dim).uniform_(-0.5 / self.e_dim,
-                                                                           0.5 / self.e_dim),
-                                                   t.zeros(1, self.e_dim)]))
+                                                      self.embedding_size).uniform_(-0.5 / self.embedding_size,
+                                                                                    0.5 / self.embedding_size),
+                                                   t.zeros(1, self.embedding_size)]))
         self.cvectors.weight = nn.Parameter(t.cat([FT(self.vocab_size - 1,
-                                                      self.e_dim).uniform_(-0.5 / self.e_dim,
-                                                                           0.5 / self.e_dim),
-                                                   t.zeros(1, self.e_dim)]))
-        self.last_item_vectors.weight = nn.Parameter(t.cat([FT(self.vocab_size - 1,
-                                                               self.e_dim).uniform_(-0.5 / self.e_dim,
-                                                                                    0.5 / self.e_dim),
-                                                     t.zeros(1, self.e_dim)]))
+                                                      self.embedding_size).uniform_(-0.5 / self.embedding_size,
+                                                                                    0.5 / self.embedding_size),
+                                                   t.zeros(1, self.embedding_size)]))
         self.tvectors.weight.requires_grad = True
         self.cvectors.weight.requires_grad = True
-        self.last_item_vectors.weight.requires_grad = True
-        self.Bt = nn.Linear(self.e_dim, self.e_dim)
-        self.W0 = nn.Linear(4 * self.e_dim, self.e_dim)
-        self.W1 = nn.Linear(self.e_dim, 1)
+        self.Ac = nn.Linear(self.embedding_size, self.d_alpha)
+        self.At = nn.Linear(self.embedding_size, self.d_alpha)
+        self.cos = nn.CosineSimilarity(dim=-1, eps=1e-6)
+        self.softmax = nn.Softmax(dim=-1)
+        self.Bc = nn.Linear(self.embedding_size, self.embedding_size)
+        self.Bt = nn.Linear(self.embedding_size, self.embedding_size)
+        self.R = nn.Linear(self.embedding_size, N * self.embedding_size)
+        self.W0 = nn.Linear(4 * self.embedding_size, self.embedding_size)
+        self.W1 = nn.Linear(self.embedding_size, 1)
         self.relu = nn.ReLU()
-        self.b_l_j = nn.Parameter(FT(self.vocab_size).uniform_(-0.5 / self.e_dim, 0.5 / self.e_dim))
+        self.b_l_j = nn.Parameter(FT(self.vocab_size).uniform_(-0.5 / self.embedding_size, 0.5 / self.embedding_size))
         self.b_l_j.requires_grad = True
-        self.attention_layers = t.nn.ModuleList()
-        self.attention_layernorms = t.nn.ModuleList()  # to be Q for self-attention
+        self.pos_bias = nn.Parameter(FT(self.window_size).uniform_(-0.5 / self.window_size,
+                                                                   0.5 / self.window_size))
+        self.pos_bias.requires_grad = True
 
-        for _ in range(num_blocks):
-            new_attn_layernorm = t.nn.LayerNorm(e_dim, eps=1e-8)
-            self.attention_layernorms.append(new_attn_layernorm)
-
-            new_attn_layer = t.nn.MultiheadAttention(e_dim,
-                                                     num_heads,
-                                                     dropout_rate,
-                                                     batch_first=True)
-            self.attention_layers.append(new_attn_layer)
+    def calc_attention(self, batch_titems, batch_citems):
+        v_l_j = self.forward_t(batch_titems)
+        u_l_m = self.forward_c(batch_citems)
+        c_vecs = self.Ac(u_l_m).unsqueeze(1)
+        t_vecs = self.At(v_l_j).unsqueeze(2)
+        cosine_sim = self.cos(t_vecs, c_vecs)
+        attention_weights = self.softmax(cosine_sim)
+        return attention_weights
 
     def forward(self, batch_titems, batch_citems, mask_pad_ids=None, inference=False):
         v_l_j = self.forward_t(batch_titems)
         u_l_m = self.forward_c(batch_citems)
-        Q = v_l_j
-        for i in range(len(self.attention_layers)):
-            if not inference:
-                Q = self.attention_layernorms[i](Q)
-                outputs, attention_weights = self.attention_layers[i](Q, u_l_m, u_l_m, key_padding_mask=mask_pad_ids)
-            else:
-                Q = self.attention_layernorms[i](v_l_j)
-                outputs, attention_weights = self.attention_layers[i](Q, u_l_m, u_l_m)
-        return outputs, attention_weights
+        c_vecs = self.Ac(u_l_m).unsqueeze(1)
+        t_vecs = self.At(v_l_j).unsqueeze(2)
+
+        cosine_sim = self.cos(t_vecs, c_vecs)
+        if [param for param in self.parameters()][0].is_cuda:
+            self.pos_bias.cuda()
+
+        batch_pos_bias = self.pos_bias.repeat(batch_titems.shape[0], batch_titems.shape[1], 1)
+        cosine_sim = cosine_sim + batch_pos_bias
+
+        if not inference:
+            cosine_sim[t.cat([mask_pad_ids] * batch_titems.shape[1], 1).view(
+                batch_titems.shape[0], batch_titems.shape[1], -1)] = -np.inf
+
+        attention_weights = self.softmax(cosine_sim)
+
+        weighted_u_l_m = t.mul(attention_weights.unsqueeze(-1), self.Bc(u_l_m).unsqueeze(1))
+
+        alpha_j_1 = weighted_u_l_m.sum(2)
+        z_j_1 = self.R(alpha_j_1)
+
+        return z_j_1
 
     def forward_t(self, data):
         v = data.long()
@@ -80,9 +90,9 @@ class AttentiveItemToVec(nn.Module):
 
 class SGNS(nn.Module):
 
-    def __init__(self, base_model, vocab_size=20000, n_negs=10, weights=None, loss_method='CCE', device='cpu'):
+    def __init__(self, ai2v, vocab_size=20000, n_negs=10, weights=None, loss_method='CCE'):
         super(SGNS, self).__init__()
-        self.ai2v = base_model
+        self.ai2v = ai2v
         self.vocab_size = vocab_size
         self.n_negs = n_negs
         self.weights = None
@@ -91,7 +101,6 @@ class SGNS(nn.Module):
             wf = wf / wf.sum()
             self.weights = FT(wf)
         self.loss_method = loss_method
-        self.device = device
 
     def similarity(self, batch_sub_users, batch_tvecs, batch_titem_ids):
         return self.ai2v.W1(self.ai2v.relu(self.ai2v.W0(t.cat([batch_sub_users, batch_tvecs,
@@ -100,17 +109,19 @@ class SGNS(nn.Module):
             self.ai2v.b_l_j[batch_titem_ids].unsqueeze(2)
 
     def inference(self, user_items):
+        if len(user_items) > self.ai2v.window_size:
+            pad_times = self.ai2v.window_size - len(user_items)
+            user_items = [self.ai2v.pad_idx] * pad_times + user_items
         num_items = self.ai2v.tvectors.weight.size()[0]
         citems = t.tensor([user_items])
+        mask_pad_ids = citems == self.ai2v.pad_idx
         all_titems = t.tensor(range(num_items)).unsqueeze(0)
-
-        citems, all_titems = citems.to(self.device), all_titems.to(self.device)
-
-        sub_user, _ = self.ai2v(all_titems, citems, mask_pad_ids=None, inference=True)
-        last_item_emb = self.ai2v.last_item_vectors(citems[:, -1].long())
-        sub_user = sub_user + last_item_emb
+        if next(self.parameters()).is_cuda:
+            citems = citems.cuda()
+            all_titems = all_titems.cuda()
+        sub_users = self.ai2v(all_titems, citems, mask_pad_ids=mask_pad_ids)
         all_tvecs = self.ai2v.Bt(self.ai2v.forward_t(all_titems))
-        sim = self.similarity(sub_user, all_tvecs, all_titems)
+        sim = self.similarity(sub_users, all_tvecs, all_titems)
         return sim.squeeze(-1).squeeze(0).detach().cpu().numpy()
 
     def forward(self, batch_titems, batch_citems, mask_pad_ids):
@@ -118,13 +129,11 @@ class SGNS(nn.Module):
             batch_nitems = t.multinomial(self.weights, batch_titems.size()[0] * self.n_negs, replacement=True).view(batch_titems.size()[0], -1)
         else:
             batch_nitems = FT(batch_titems.size()[0], self.n_negs).uniform_(0, self.vocab_size - 1).long()
-        batch_nitems = batch_nitems.to(self.device)
+        if next(self.parameters()).is_cuda:
+            batch_nitems = batch_nitems.cuda()
 
         batch_titems = t.cat([batch_titems.reshape(-1, 1), batch_nitems], 1)
-        batch_last_items = self.ai2v.last_item_vectors(batch_citems[:, -1])
-        batch_last_items = batch_last_items.unsqueeze(1).repeat(1, batch_titems.shape[1], 1)
-        batch_sub_users, _ = self.ai2v(batch_titems, batch_citems, mask_pad_ids)
-        batch_sub_users = batch_sub_users + batch_last_items
+        batch_sub_users = self.ai2v(batch_titems, batch_citems, mask_pad_ids)
         batch_tvecs = self.ai2v.Bt(self.ai2v.forward_t(batch_titems))
         if [param for param in self.ai2v.parameters()][0].is_cuda:
             self.ai2v.b_l_j.cuda()
@@ -133,39 +142,17 @@ class SGNS(nn.Module):
 
         if self.loss_method == 'CCE':  # This option is the default option.
             soft = sim.softmax(dim=1) + 1e-6
+            print(-soft[:, 0].log().sum())
             return -soft[:, 0].log().sum()
 
         if self.loss_method == 'BCE':
             soft_pos = sim[:, 0].sigmoid() + 1e-6
             soft_neg = sim[:, 1:].neg().sigmoid() + 1e-6
+            print((-soft_pos.log().sum()) + (-soft_neg.log().sum()))
             return (-soft_pos.log().sum()) + (-soft_neg.log().sum())
 
         if self.loss_method == 'Hinge':
             soft_pos = t.maximum((t.ones_like(sim[:, 0]) - sim[:, 0]), t.zeros_like(sim[:, 0])) + 1e-6
             soft_neg = t.maximum((t.ones_like(sim[:, 1:]) - (-sim[:, 1:])), t.zeros_like(sim[:, 1:])) + 1e-6
+            print(soft_pos.sum() + soft_neg.sum())
             return soft_pos.sum() + soft_neg.sum()
-
-        else:
-            raise NotImplementedError
-
-    def run_epoch(self, train_dl, epoch, sgns, optim, pad_idx):
-        pbar = tqdm(train_dl)
-        pbar.set_description("[Epoch {}]".format(epoch))
-        train_loss = 0
-
-        srt = datetime.datetime.now().replace(microsecond=0)
-        for batch_titems, batch_citems in pbar:
-            batch_titems, batch_citems = batch_titems.to(self.device), batch_citems.to(self.device)
-            mask_pad_ids = (batch_citems == pad_idx)
-            loss = sgns(batch_titems, batch_citems, mask_pad_ids)
-            train_loss += loss.item()
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-            pbar.set_postfix(train_loss=loss.item())
-
-        train_loss = train_loss / len(pbar)
-        print(f'train_loss: {train_loss}')
-        end = datetime.datetime.now().replace(microsecond=0)
-        print('epoch time: ', end - srt)
-        return train_loss, sgns
