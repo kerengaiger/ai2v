a4e6c9080c55b9a11b5cd6167d4e3d0b514ee32f
kerengaiger <keren.gaiger@gmail.com>
2021-10-14 12:11:11 +0300

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# # this installs the right pip and dependencies for the fresh python
 conda install ipython pip
 conda install python=3.7
 
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 
-# dependencies for ax
-pip install scipy botorch jinja2 pandas plotly
+pip install scipy botorch jinja2 pandas plotly optuna tensorboard tqdm
 
-# install ax platform
-conda install -c conda-forge optuna
-
-# for tensorboard
-pip install tensorboard
-
-pip install tqdm
 
 
