0d68ee7ff1a2e62020cf8de69a50b9e4e1ade142
kerengaiger <keren.gaiger@gmail.com>
2021-10-20 15:08:56 +0300

    install: add scikit-learn dependency

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ conda install python=3.7
 
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 
-pip install scipy botorch jinja2 pandas plotly optuna tensorboard tqdm
+pip install scipy botorch jinja2 pandas plotly optuna tensorboard tqdm scikit-learn
 
 
 
