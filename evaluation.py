2bf04dbbdc2fa331648ce52ad07561a76a20f61b
kerengaiger <keren.gaiger@gmail.com>
2022-03-12 20:19:47 +0200

    evaluation: iterate ove user_ids

--- evaluation.py
+++ evaluation.py
@@ -56,8 +56,8 @@ def predict(model, eval_set_lst, eval_set_df, out_file):
     pbar = tqdm(eval_set_lst)
     model.eval()
     eval_set_df['pred_loc'] = np.nan
-    for i, (user_itemids, target_item) in enumerate(pbar):
-        items_ranked = model.inference(user_itemids).argsort()
+    for i, (user_id, user_itemids, target_item) in enumerate(pbar):
+        items_ranked = model.inference(user_id, user_itemids).argsort()
         all_items = items_ranked[:][::-1]
         loc = np.where(all_items == target_item)[0][0] + 1
         eval_set_df.loc[i, 'pred_loc'] = loc
